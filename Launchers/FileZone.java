bc95b6fd2b8a3dde0494a2fc012d1b8dd32cadac
insane-z41n <49139582+insane-z41n@users.noreply.github.com>
2019-04-07 11:06:02 -0500

    Create FileZone.java
    Launches ConnectFrame.java

---
+++ Launchers/FileZone.java
@@ -0,0 +1,10 @@
+package Launchers;
+import Connect.ConnectFrame;
+
+public class FileZone {
+	
+	public static void main(String [] args) {
+		ConnectFrame conFrame = new ConnectFrame(600, 400);
+		conFrame.initFrame();
+	}
+}
